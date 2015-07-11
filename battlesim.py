6cd4606f14ad89e7b05c2158d7c19a009bec7af5
Shashwat Chandra <chandra.shashwat@gmail.com>
2015-07-11 20:13:37 +0530

    Modified - added str to dmg, adding momentum - in progress

--- battlesim.py
+++ battlesim.py
@@ -22,6 +22,7 @@ class Player(object):
 		self.stre = stre
 		self.coun = coun
 		self.HP = tough * 6 * 4
+		self.maxHP = self.HP
 		self.degree = 1
 	
 	def move(self):
@@ -48,6 +49,7 @@ class Fight(object):
 		if atk_ > def_ :
 			p2.HP -= d1
 
+	#def dmg(pa,pd,sa,da,ta,sd,dd,td)
 	def fight(self):
 		ctr = 1
 		p1 = self.p1
@@ -60,17 +62,26 @@ class Fight(object):
 			atk_ = dieroll(p1.stre) + s1 + matching[(t1,t2)]
 			def_ = dieroll(p2.coun) + s2
 			if atk_ > def_ :
-				p2.HP -= d1
+				p2.HP -= d1 + dieroll(p1.stre)
+			else:
+				print "Counter 1!"
 			
 			# 2's attack
 			atk_ = dieroll(p2.stre) + s2 + matching[(t2,t1)]
 			def_ = dieroll(p1.coun) + s1
 			if atk_ > def_ :
-				p1.HP -= d2
+				p1.HP -= d2 + dieroll(p2.stre)
+			else:
+				print "Counter 2!"
 
+			# Recovery
 			p1.HP += dieroll(p1.stam)
+			if p1.HP > p1.maxHP: p1.HP = p1.maxHP
+
 			p2.HP += dieroll(p2.stam)
+			if p2.HP > p2.maxHP: p2.HP = p2.maxHP
 			
+			# Degree
 			a = dieroll(1)
 			if a > 4:
 				if dieroll(1) > p1.status():
@@ -94,24 +105,44 @@ class Fight(object):
 			if p1.degree < 0: p1.degree = 0
 			if p2.degree < 0: p2.degree = 0
 			
-			#print "Turn {}: P1 HP = {}; P2 HP = {}".format(ctr,p1.HP, p2.HP)
+			if p1.degree > 5: p1.degree = 5
+			if p2.degree > 5: p2.degree = 5
+
+			#print "h"
+			if p1.HP - p2.HP > self.underdog:
+				print "Underdog"
+				p2.degree = 5
+			if p2.HP - p1.HP > self.underdog:
+				print "Underdog"
+				p1.degree = 5
+			
+			
+			print "Turn {}: P1 sd = {}/{}; P2 sd = {}/{}".format(ctr,s1,d1, s2,d2)
+			print "Turn {}: P1 HP = {}; P2 HP = {}".format(ctr,p1.HP, p2.HP)
+			print "Turn {}: P1 dg = {}; P2 dg = {}".format(ctr,p1.degree, p2.degree)
+			print
 			ctr += 1
-		#print "Player {} Wins!".format(1 if p1.HP > p2.HP else 2)
+		print "Player {} Wins!".format(1 if p1.HP > p2.HP else 2)
 		return 1 if p1.HP > p2.HP else 2, ctr
 
 ctr = 0
 ctrc = 0
-for i in range(1000):
-	p1 = Player(2,4,2,2)
+N = 100
+for i in range(N):
+	p1 = Player(4,2,2,2)
 	p2 = Player(2,3,2,3)
 	
+	print "Starting conditions: P1 = {}, {} ; P2 = {}, {}".format(p1.HP,p1.degree,p2.HP,p2.degree)
+
 	f=Fight(p1,p2)
+	print "Underdog:",f.underdog
+	print
 	p,c = f.fight()
 	ctr += p
 	ctrc += c
 
-print ctr * 1.0/1000
-print ctrc * 1.0/1000
+print ctr * 1.0/N
+print ctrc * 1.0/N
 
 
 
